aea22e2135455e5ebe978ae505cf41ffb2efb969
Daniel Pereira <54162368+Dannyp425@users.noreply.github.com>
2021-02-01 11:40:42 -0700

    First steps

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+var name = "your name";
+console.log(name);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
